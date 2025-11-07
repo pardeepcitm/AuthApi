d2377bf5033caeee7fd76a68cf34f3539d298dcf
pardeepcitm <63185204+pardeepcitm@users.noreply.github.com>
2025-11-08 00:32:42 +0100

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,52 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+    public DbSet<User> Users { get; set; }
+}
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    public string PasswordHash { get; set; }
+
+    // For Forgot Password
+    public string? ResetToken { get; set; }
+    public DateTime? ResetTokenExpires { get; set; }
+}
+public class RegisterRequest
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+    
+public class LoginRequest
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
+    
+public class ForgotPasswordRequest
+{
+    public string Email { get; set; } = string.Empty;
+}
+public class ResetPasswordRequest
+{
+    public string Token { get; set; } = string.Empty;
+    public string NewPassword { get; set; } = string.Empty;
+}
